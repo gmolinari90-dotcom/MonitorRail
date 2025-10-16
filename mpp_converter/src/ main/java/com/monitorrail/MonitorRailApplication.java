9b2bb3ed0d0fd968e6a19b79829395c6447c44bf
gmolinari90-dotcom <g.molinari90@gmail.com>
2025-10-16 10:38:17 +0200

    Update MonitorRailApplication.java

--- mpp_converter/src/ main/java/com/monitorrail/MonitorRailApplication.java
+++ mpp_converter/src/ main/java/com/monitorrail/MonitorRailApplication.java
@@ -1,1 +1,12 @@
+package com.monitorrail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorRailApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MonitorRailApplication.class, args);
+    }
+}
 
